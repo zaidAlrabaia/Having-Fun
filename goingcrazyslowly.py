bda34c4e3681a5e62fc713956f199035a8ac7322
zaidAlrabaia <alrabaia@kth.se>
2025-06-08 01:46:12 +0200

    another slow day

--- goingcrazyslowly.py
+++ goingcrazyslowly.py
@@ -27,10 +27,58 @@ def searching(nums, element):
 def searching_index (nums, element): #Fucked up this one
     if not nums:
         return -1
-    if nums[0] != element:
-        return 1 + searching_index(nums[1:], element)
     if nums[0] == element:
         return 0 
+    sub_index = searching_index(nums[1:], element)
+    if sub_index == -1:
+        return -1
+    else:
+        return 1 + sub_index
+
+def count_occurences (nums,element):
+    if not nums:
+        return 0
+    return (1 if nums[0] == element else 0) + count_occurences(nums[1:], element)
+
+def is_palindrome (s:str) -> bool:
+    if len(s) == 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    else:
+        return is_palindrome(s[1:-1])
+    
+def sum_list (nums):
+    if not nums:
+        return 0
+    else:
+        return nums[0] + sum_list(nums[1:])
+    
+def Fibonacci (num: int) -> int:
+    if num <= 1:
+        return
+    else:
+        return Fibonacci(num -1) + Fibonacci(num-2)
+
+def binary_search (nums, element, left, right):
+    if left > right:
+        return -1
+    middle = (right +  left) // 2
+    if nums[middle] == element:
+        return middle
+    elif nums[middle] < element:
+        return binary_search(nums, element, middle + 1, right)
+    else:
+        return binary_search(nums, element, left, middle -1)
+    
+    
+
+    
+
+listt = [1,2,3,4,5,6,7]
+
+print(is_palindrome("False"))
+print(searching_index(listt, 5))
 
     
 
